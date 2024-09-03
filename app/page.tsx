ca5bfb0c0ee944fe4f0a6bc337f437ed884b76ba
rishijain2002 <rishijain0652@gmail.com>
2024-09-03 21:16:41 +0530

    change file name

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import SearchBox from "@/component/SearchBox";
-import { Register } from "./Register/page";
+import { Register } from "./register/page";
 
 export default function Home() {
   return (
