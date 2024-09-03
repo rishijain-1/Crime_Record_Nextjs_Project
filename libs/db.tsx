46b28718aaf4333733e23436a4465ca62bd9ce5d
rishijain2002 <rishijain0652@gmail.com>
2024-09-03 20:47:49 +0530

    make JWT Function

--- libs/db.tsx
+++ libs/db.tsx
@@ -4,7 +4,7 @@ const MONGO_URI = process.env.MONGO_URI || 'your-mongodb-connection-string';
 
 const connectMongo = async () => {
   if (mongoose.connection.readyState === 1) {
-    return; // Connection is already established
+    return; 
   }
 
   try {
