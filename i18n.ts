2250efcf2e073d458d7440a99afa5813f4796071
rishijain2002 <rishijain0652@gmail.com>
2024-09-10 19:52:35 +0530

    moidfy

--- i18n.ts
+++ i18n.ts
@@ -1,9 +1,10 @@
 import { notFound } from "next/navigation";
 import { getRequestConfig } from "next-intl/server";
+import { routing } from "./navigation";
 
 export const locales=["en", "hi"];
 export default getRequestConfig(async({locale})=>{
-    if(!locales.includes(locale as any)) notFound();
+    if(!routing.locales.includes(locale as any)) notFound();
     const messages = await import(`/messages/${locale}.json`);
     return {
         messages: messages.default,
