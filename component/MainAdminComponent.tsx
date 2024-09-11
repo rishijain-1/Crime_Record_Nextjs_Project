f716535f96efe5904957c4ee2f664476520ee1cc
rishijain2002 <rishijain0652@gmail.com>
2024-09-11 18:51:25 +0530

    make functionality for admin accessibility

--- component/MainAdminComponent.tsx
+++ component/MainAdminComponent.tsx
@@ -1,7 +1,39 @@
+"use client"
+import { useRouter } from '@/navigation';
 import React, { useEffect, useState } from 'react'
+import Loading from './Loader/Loading';
 import AdminSearchBox from './AdminSearchBox';
+import { getCurrentUser } from '@/app/api/session';
 
 const MainAdminComponent = () => {
+    const router = useRouter();
+    const [user, setUser] = useState<any>(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    useEffect(() => {
+        const fetchUser = async () => {
+          const session = await getCurrentUser();
+
+          if (session?.user?.role === 'admin') {
+            setUser(session?.user);
+          } else {
+            setError('Unauthorized');
+            router.push('/login');
+          }
+    
+          setLoading(false);
+        };
+    
+        fetchUser();
+      }, [router]);
+
+      if (loading) {
+        return <div className="items-center h-screen flex justify-center"><Loading/></div>;
+      }
+    
+      if (error) {
+        return <div className="items-center h-screen flex justify-center">{error}</div>;
+      }
     
   return (
     <AdminSearchBox/>
