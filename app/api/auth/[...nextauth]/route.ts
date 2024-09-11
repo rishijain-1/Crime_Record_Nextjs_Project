1bd691c6784469cacc06a7e11155fd013c7dd1a8
rishijain2002 <rishijain0652@gmail.com>
2024-09-11 17:28:02 +0530

    modify file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,53 +1,8 @@
-import NextAuth, { NextAuthOptions } from 'next-auth';
-import CredentialsProvider from 'next-auth/providers/credentials';
-import bcrypt from 'bcryptjs';
-import connectMongo from '@/libs/db';
-import User from '@/model/userModel';
+import NextAuth from "next-auth";
+import { authOptions } from "./option";
 
-export const authOptions: NextAuthOptions = {
-  session: {
-    strategy: 'jwt',
-  },
-  providers: [
-    CredentialsProvider({
-      name: 'Credentials',
-      credentials: {
-        email: { label: 'Email', type: 'email' },
-        password: { label: 'Password', type: 'password' },
-      },
-      async authorize(credentials) {
-        await connectMongo();
-
-        const user = await User.findOne({ email: credentials?.email });
-
-        if (!user || !(await bcrypt.compare(credentials?.password || '', user.password))) {
-          throw new Error('Invalid credentials');
-        }
-
-        return user;
-      },
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, user }) {
-      if (user) {
-        token.role = user.role;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      if (token?.role) {
-        session.user.role = token.role;
-      }
-      return session;
-    },
-  },
-  pages: {
-    signIn: '/en/login' || '/hi/login', // Ensure you handle localization in your routing setup
-  },
-};
-
-// Handler for both GET and POST requests
+// The actual handler for NextAuth
 const handler = NextAuth(authOptions);
 
+// Export the handler for both GET and POST methods
 export { handler as GET, handler as POST };
